b50bcf7a639f788acde9be1af0df2b2e2d6428fc
Alfred Cepeda <AlfredJCepeda@gmail.com>
2016-02-21 08:18:51 +0000

    Adds didGet and didSet hooks

--- src/kvo.js
+++ src/kvo.js
@@ -1,56 +1,62 @@
 var KVO = function() { 
 	this.bound = {};
-	this.observer = {};
+	this.observers = {};
 };
 
 KVO.prototype.setKey = "_kvo";
 
-KVO.prototype.didSet = function(id, prop, value) {
-
+KVO.prototype.do_didSet = function(id, prop, value) {
+	this.observers[id].didSet[prop].forEach(function(obs) {
+		obs(value);
+	});
 };
 
-KVO.prototype.didGet = function(id, prop, value) {
-	
-};
+KVO.prototype.do_didGet = function(id, prop, value) {
+	this.observers[id].didGet[prop].forEach(function(obs){
+		obs(value);
+	});
+};;
+
+//KVO.prototype.didSet = function()
 
 KVO.prototype.wrapSetter = function(id, prop, setter) {
 	var self = this;
 
 	return function(value) {
 		setter.call(this, value);
-		self.didSet(this, prop, value);
+		self.do_didSet(id, prop, value);
 	};
 };
 
-KVO.prototype.generateSetter = function(obj, prop) {
+KVO.prototype.generateSetter = function(id, prop) {
 	var self = this;
 	var _kvo = this.setKey;
 
 	return function(value) {
 		this[_kvo][prop] = value;
-		self.didSet(this, prop, value);
-	}.bind(obj);
+		self.do_didSet(id, prop, value);
+	};
 };
 
-KVO.prototype.wrapGetter = function(obj, prop, getter) {
+KVO.prototype.wrapGetter = function(id, prop, getter) {
 	var self = this;
 
 	return function() {
 		var value = getter.call(this);
-		self.didGet(this, prop, value);
+		self.do_didGet(id, prop, value);
 		return value;
-	}.bind(obj);
+	};
 };
 
-KVO.prototype.generateGetter = function(obj, prop) {
+KVO.prototype.generateGetter = function(id, prop) {
 	var self = this;
 	var _kvo = this.setKey;
 
 	return function() {
 		var value = this[_kvo][prop];
-		self.didGet(this, prop, value);
+		self.do_didGet(id, prop, value);
 		return value;
-	}.bind(obj);
+	};
 };
 
 KVO.prototype.convert = function(obj, prop) {
@@ -73,17 +79,19 @@ KVO.prototype.convert = function(obj, prop) {
 	}
 
 	var _kvo = this.setKey;
-	if(typeof obj[_kvo][prop] === "undefined") {
-		obj[_kvo][prop] = obj[prop];
-	}
+	var id = obj[_kvo]["_kvoid"];
+
+	obj[_kvo][prop] = obj[prop];
+	this.observers[id].didSet[prop] = [];
+	this.observers[id].didGet[prop] = [];
 
 	var setter, getter;
 	if(hasSet && hasGet) {
-		setter = this.wrapSetter(obj, prop, descriptor.set);
-		getter = this.wrapGetter(obj, prop, descriptor.get);
+		setter = this.wrapSetter(id, prop, descriptor.set);
+		getter = this.wrapGetter(id, prop, descriptor.get);
 	} else {
-		setter = this.generateSetter(obj, prop);
-		getter = this.generateGetter(obj, prop);
+		setter = this.generateSetter(id, prop);
+		getter = this.generateGetter(id, prop);
 	}
 
 	Object.defineProperty(obj, prop, { 
@@ -107,10 +115,15 @@ KVO.prototype.bind = function(obj) {
 	var id;
 	do {
 		id = Math.random().toString(36).substr(2, 5);
-	}while(typeof this.bound[id] !== "undefined")
+	}while(typeof this.bound[id] !== "undefined");
 
 	var _kvo = this.setKey;
 	obj[_kvo] = { "_kvoid":id }; 
 	this.bound[id] = obj;
+
+	this.observers[id] = {
+		didSet:{},
+		didGet:{}
+	};
 }
 
